63bc626a322a16b53768732596a1cfc12851ebbe
liuyuxi <915988179@qq.com>
2025-10-22 20:45:16 +0800

    1

---
+++ boot/loader.asm
@@ -0,0 +1,122 @@
+%include "boot.inc"
+
+section loader vstart=LOADER_BASE_ADDR
+LOADER_STACK_TOP equ LOADER_BASE_ADDR
+jmp loader_start
+
+; 构建 gdt 及其内部的描述符
+GDT_BASE: dd 0x00000000
+          dd 0x00000000
+
+CODE_DESC: dd 0x0000FFFF
+           dd DESC_CODE_HIGH4
+
+DATA_STACK_DESC: dd 0x0000FFFF
+                 dd DESC_DATA_HIGH4
+
+VIDEO_DESC: dd  0x0000B800   ; limit=(0xbffff-0xb8000)/4k=0x7
+           dd DESC_VIDEO_HIGH4 ; 此时 dpl 为 0
+
+GDT_SIZE equ $ - GDT_BASE
+GDT_LIMIT equ GDT_SIZE - 1
+times 60 dq 0 ; 此处预留 60 个描述符的空位
+
+SELECTOR_CODE equ (0x0001<<3) + TI_GDT + RPL0
+SELECTOR_DATA equ (0x0002<<3) + TI_GDT + RPL0
+SELECTOR_VIDEO equ (0x0003<<3) + TI_GDT + RPL0
+
+; 以下是 gdt 的指针，前 2 字节是 gdt 界限，后 4 字节是 gdt 起始地址
+gdt_ptr dw GDT_LIMIT
+        dd GDT_BASE
+
+loadermsg db '2 loader in real.'
+
+loader_start:
+;------------------------------------------------------------
+; INT 0x10 功能号:0x13 功能描述:打印字符串
+;------------------------------------------------------------
+; 输入:
+; AH 子功能号=13H
+; BH = 页码
+; BL = 属性(若 AL=00H 或 01H)
+; CX=字符串长度
+; (DH, DL)=坐标(行, 列)
+; ES:BP=字符串地址
+; AL=显示输出方式
+; 0—字符串中只含显示字符，其显示属性在 BL 中 显示后，光标位置不变
+; 1—字符串中只含显示字符，其显示属性在 BL 中 显示后，光标位置改变
+; 2—字符串中含显示字符和显示属性。显示后，光标位置不变
+; 3—字符串中含显示字符和显示属性。显示后，光标位置改变
+; 无返回值
+
+mov sp, LOADER_BASE_ADDR
+mov bp, loadermsg ; ES:BP = 字符串地址
+mov cx, 17        ; CX = 字符串长度
+mov ax, 0x1301    ; AH = 13, AL = 01h
+mov bx, 0x001f    ; 页号为 0(BH=0) 蓝底粉红字(BL=1fh)
+mov dx, 0x1800
+int 0x10          ; 10h 号中断
+
+; --------------------
+; 准备进入保护模式
+; --------------------
+; 1. 打开 A20
+; 2. 加载 GDT
+; 3. 将 CR0 的 PE 位置 1
+
+
+
+
+;----------------- 打开 A20 ----------------
+in al,0x92
+or al,0000_0010B
+out 0x92,al
+
+;打印1
+mov byte [loadermsg], '1'
+mov sp, LOADER_BASE_ADDR
+mov bp, loadermsg
+mov cx,1
+mov ax,0x1301
+mov bx,0x001F       ; 页0, 蓝底粉红字
+mov dx,0x0000       ; 行0，列0
+int 0x10
+;----------------- 加载 GDT ----------------
+lgdt [gdt_ptr]
+mov byte [loadermsg], '2'
+mov bp, loadermsg
+mov cx,1
+mov dx,0x0001       ; 行0，列1
+int 0x10
+
+;----------------- CR0 第 0 位置 1 ----------------
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+
+mov byte [gs:4], '3'
+mov byte [gs:5], 0x1F
+
+jmp SELECTOR_CODE:p_mode_start ; 刷新流水线
+
+
+[bits 32]
+p_mode_start:
+
+mov ax, SELECTOR_DATA
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov esp, LOADER_STACK_TOP
+
+mov ax, SELECTOR_VIDEO
+mov gs, ax
+nop
+nop
+
+mov byte [0xB8000], 'P'
+mov byte [0xB8001], 0x1F
+mov byte [0xB8002], 'Q'
+mov byte [0xB8003], 0x1F
+
+jmp $
---
+++ boot/mbr.asm
@@ -0,0 +1,99 @@
+                                ;主引导程序
+                                ;----------
+%include "boot.inc"
+ 
+                                ;SECTION MBR vstart=0x7c00
+bits 16
+org 0x7c00
+mov ax, cs
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov fs, ax
+mov sp, 0x7c00
+mov ax, 0xb800
+mov gs, ax
+
+mov ax, 0600h
+mov bx, 0700h
+mov cx, 0      
+mov dx, 184fh  
+
+int 10h
+mov byte [gs:0x00],'1'
+mov byte [gs:0x01],0xA4  
+
+mov byte [gs:0x02],' '
+mov byte [gs:0x03],0xA4
+
+mov byte [gs:0x04],'M'
+mov byte [gs:0x05],0xA4
+
+mov byte [gs:0x06],'B'
+mov byte [gs:0x07],0xA4
+
+mov byte [gs:0x08],'R'
+mov byte [gs:0x09],0xA4
+
+mov eax,LOADER_START_SECTOR         ;起始扇区lba地址
+mov bx,LOADER_BASE_ADDR             ;写入的地址
+mov cx,4                           ;待读入的扇区数
+call rd_disk_m_16                   ;以下读取程序的起始部分，call入栈指令地址
+
+jmp LOADER_BASE_ADDR
+
+rd_disk_m_16:
+
+mov esi,eax                         ;备份数据
+mov di,cx
+
+mov dx,0x1f2                        ;1.选择特定通道的寄存器
+mov al,cl                       
+out dx,al
+
+mov eax,esi             
+
+mov dx,0x1f3                         ;2.在特定通道寄存器中放入要读取的扇区地址
+out dx,al
+
+mov cl,8                            ;以下为将lba地址写入dx中的端口，每次写完并右移cx（8位）
+shr eax,cl
+mov dx,0x1f4
+out dx,al
+
+shr eax,cl
+mov dx,0x1f5
+out dx,al
+
+shr eax,cl
+and al,0x0f
+or al,0xe0
+mov dx,0x1f6
+out dx,al
+
+mov dx,0x1f7                            ;3.向0x1f7写入0x20命令
+mov al,0x20
+out dx,al
+
+not_ready:                              ;4.检查硬盘状态
+nop                                     ;空转
+in al,dx                                ;读取statu寄存器，判断硬盘状态，第四位为1表示准备好了，第七位为一表示忙
+and al,0x88
+cmp al,0x08
+jnz not_ready
+
+mov ax,di                               ;5.从0x1f0读数据
+mov dx,256                              ;di存储的是要读取的扇区数
+mul dx
+mov cx,ax
+
+mov dx,0x1f0
+go_on_read:
+in ax,dx
+mov [bx],ax
+add bx,2
+loop go_on_read
+ret
+
+times 510-($-$$) db 0
+db 0x55,0xaa
