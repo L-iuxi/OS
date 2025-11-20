dd11b72b48eb3e6a93a4a422e4216621f0e2eb74
liuyuxi <915988179@qq.com>
2025-11-20 16:26:40 +0800

    put_char

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,19 @@
+#include "../include/kernel/print.h"
 int main(void)
 {
+	put_char('K');
+	put_char('E');
+	put_char('R');
+	put_char('R');
+	put_char('N');
+	put_char('E');
+	put_char('L');
+	put_char('\n');
+	put_char('1');
+	put_char('2');
+	put_char('\b');
+	put_char('3');
 	while(1);
-	return 0;
+	
 }
 
