d72c262d53ae71349baae293a000ed0fff0789a2
liuyuxi <915988179@qq.com>
2025-11-24 15:42:32 +0800

    put_str

--- kernel/main.c
+++ kernel/main.c
@@ -1,18 +1,26 @@
 #include "../include/kernel/print.h"
 int main(void)
 {
-	put_char('K');
-	put_char('E');
-	put_char('R');
-	put_char('R');
-	put_char('N');
-	put_char('E');
-	put_char('L');
-	put_char('\n');
-	put_char('1');
-	put_char('2');
-	put_char('\b');
-	put_char('3');
+	
+put_str("I am kernel\n");
+
+put_int(0);
+
+put_char('\n');
+
+put_int(9);
+
+put_char('\n');
+
+put_int(0x00021a3f);
+
+put_char('\n');
+
+put_int(0x12345678);
+
+put_char('\n');
+
+put_int(0x00000000);
 	while(1);
 	
 }
