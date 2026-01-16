4a2bfa5b1c62b609d87ee012e73bb6587da41e9b
幸福的烩面 <915988179@qq.com>
2026-01-16 17:05:31 +0800

    time

--- kernel/init.c
+++ kernel/init.c
@@ -1,9 +1,11 @@
+#include "../include/kernel/time.h"
 #include "../include/kernel/init.h"
 #include "../include/kernel/print.h"
 #include "../include/kernel/interrupt.h"
 
 /*负责初始化所有模块 */
 void init_all() {
-put_str("init_all\n");
-idt_init();//初始化中断
+   put_str("init_all\n");
+   idt_init();   //初始化中断
+   timer_init();
 }
