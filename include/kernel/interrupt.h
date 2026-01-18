122274533d6058323d80b7a58e4bdefc803a3feb
幸福的烩面 <915988179@qq.com>
2026-01-18 18:28:25 +0800

    thread

--- include/kernel/interrupt.h
+++ include/kernel/interrupt.h
@@ -1,10 +1,8 @@
 #ifndef __KERNEL_INTERRUPT_H
 #define __KERNEL_INTERRUPT_H
+#include "stdint.h"
 typedef void* intr_handler;		//将intr_handler定义为void*同类型
-enum intr_status intr_get_status(void);
-enum intr_status intr_set_status (enum intr_status);
-enum intr_status intr_enable (void);
-enum intr_status intr_disable (void);
+void idt_init(void);
 
 /* 定义中断的两种状态:
  * INTR_OFF值为0,表示关中断,
@@ -14,5 +12,9 @@ enum intr_status {		 // 中断状态
     INTR_ON		         // 中断打开
 };
 
+enum intr_status intr_get_status(void);
+enum intr_status intr_set_status (enum intr_status);
+enum intr_status intr_enable (void);
+enum intr_status intr_disable (void);
+void register_handler(uint8_t vector_no, intr_handler function);
 #endif
-
